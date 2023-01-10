02adf56628c6f6f9eacfb6116f71abac935e4d39
kslee <ks@predict.kr>
2023-01-10 21:23:35 +0900

    socket.on('chat message', function(msg) {}); 	modified:   index.html io.emit('chat message', msg); 	modified:   index.js

--- index.js
+++ index.js
@@ -11,16 +11,18 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
+    // socket.broadcast.emit('hi');
     console.log('a user connected');
 
     socket.on('disconnect', () => {
       console.log('user disconnected');
     });
 
-  socket.on('chat message', (msg) => {
+    socket.on('chat message', (msg) => {
     console.log('message: ' + msg);
-  });
-  
+    io.emit('chat message', msg);
+    });
+
 });
 
 server.listen(3000, () => {

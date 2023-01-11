2cfc489c6bae63f39fa5fef6583ad608b8071b4a
kslee <ks@predict.kr>
2023-01-11 17:53:05 +0900

    init new tutorial project 	new file:   client/client.js 	new file:   client/index.html 	new file:   client/package-lock.json 	new file:   client/package.json 	new file:   server/package-lock.json 	new file:   server/package.json 	new file:   server/server.js

---
+++ client/client.js
@@ -0,0 +1,26 @@
+const joinRoomButton = document.getElementById("room-button")
+const messageInput = document.getElementById("message-input")
+const roomInput = document.getElementById ("room-input")
+const form = document.getElementById("form")
+
+form.addEventListener("submit", e => {
+  e.preventDefault()
+  const message = messageInput.value
+  const room = roomInput.value
+
+  if (message === "") return
+  displayMessage(message)
+
+  messageInput.value = ""
+})
+
+joinRoomButton.addEventListener("click", () => {
+  const room = roomInput.value
+})
+
+function displayMessage (message) {
+  var item = document.createElement('li');
+  item.textContent = message;
+  document.getElementById("message-container").appendChild(item);
+  window.scrollTo(0, document.body.scrollHeight);
+}

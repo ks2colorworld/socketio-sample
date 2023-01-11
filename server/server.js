6c70448687119e65bd554999224f0118c5f3be5f
kslee <ks@predict.kr>
2023-01-11 20:23:33 +0900

    CORS / socket.broadcast.emit 	modified:   client/index.html 	modified:   server/server.js

--- server/server.js
+++ server/server.js
@@ -1,5 +1,24 @@
-const io = require('socket.io')(3001)
+const io = require('socket.io')(3001, {
+    cors: {
+        origin:[
+            'http://localhost:8080'
+        ]
+    }
+})
 
 io.on('connection',socket=>{
     console.log(socket.id);
+
+    /* (1)
+    socket.on('custom-event', (num, str, obj)=>{
+        console.log(num, str, obj);
+    });
+    //  */
+
+    socket.on('send-message', (message)=>{
+        // io.emit('receive-message', message); // >> send all
+        socket.broadcast.emit('receive-message', message); // >> send all except me
+
+        // console.log(message);
+    });
 })
